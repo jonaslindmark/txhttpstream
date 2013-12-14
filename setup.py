d2644fcaa0678d68577211975327e983ebc1fb6c
Jonas Lindmark <lindmark@wrapp.com>
2013-12-14 19:36:57 +0100

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name='txhttpstream',
     version='1.0',
     packages=['txhttpstream'],
+    install_requires=["Twisted", "nose"],
     data_files=[],
     zip_safe=False,
     test_suite='tests',
